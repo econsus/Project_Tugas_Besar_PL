3843790debd03e311c778212520422ce609fef6f
Hammam Abid <mha150720@gmail.com>
2023-01-02 14:53:41 +0700

    kenapa kok bisa ini

--- src/Driver.java
+++ src/Driver.java
@@ -1,7 +1,10 @@
 public class Driver {
     public static void main(String[] args) {
-    delete_buku deleteBuku = new delete_buku();
-    deleteBuku.display(deleteBuku);
-    deleteBuku.deleteButton();
+//    delete_buku deleteBuku = new delete_buku();
+//    deleteBuku.display(deleteBuku);
+//    deleteBuku.deleteButton();
+        Input_Data input_data = new Input_Data();
+        input_data.display(input_data);
+        input_data.input_Button();
     }
 }
--- src/Input_Data.java
+++ src/Input_Data.java
@@ -1,11 +1,39 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Input_Data {
-    private JTextField textField1;
-    private JTextField textField2;
-    private JTextField textField3;
-    private JTextField textField4;
-    private JTextField textField5;
+public class Input_Data extends JFrame{
+    private JTextField fieldJudul;
+    private JTextField fieldTahun;
+    private JTextField fieldPenerbit;
+    private JTextField fieldPengarang;
+    private JTextField fieldRak;
     private JButton inputButton;
     private JPanel Input_Data;
+
+    private DatabaseManager databaseManager = new DatabaseManager();
+
+    public void display (Input_Data screen) {
+        screen.setContentPane(screen.Input_Data);
+        screen.setTitle("Testing");
+        screen.setSize(400, 400);
+        screen.setVisible(true);
+        screen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        databaseManager.connect();
+    }
+
+    public void input_Button() {
+        inputButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String judul, pengarang, penerbit, tahun, rak;
+                judul = fieldJudul.getText();
+                pengarang = fieldPengarang.getText();
+                penerbit = fieldPenerbit.getText();
+                tahun = fieldTahun.getText();
+                rak = fieldRak.getText();
+                databaseManager.exportData(judul, pengarang, penerbit, tahun, rak);
+            }
+        });
+    }
 }
