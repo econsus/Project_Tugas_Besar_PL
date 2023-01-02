ccf8b69dc8ba1bf750d6f79277f6ac415d2ab008
Hammam Abid <mha150720@gmail.com>
2023-01-02 11:48:23 +0700

    add update, hapus, dan select data

--- src/DatabaseManager.java
+++ src/DatabaseManager.java
@@ -42,4 +42,59 @@ public class DatabaseManager {
         }
         return 0;
     }
+
+    public void updateData (String judul, String pengarang, String penerbit,String tahun , String rak, String target) {
+        try {
+            preparedStatement = connection.prepareStatement("UPDATE databuku SET `Judul Buku` = ?, `Pengarang Buku` = ?, `Penerbit Buku` = ?, `Tahun Terbit` = ?, `Nomor Rak` = ?, WHERE `Judul Buku` = ?");
+            preparedStatement.setString(1, judul);
+            preparedStatement.setString(2, pengarang);
+            preparedStatement.setString(3, penerbit);
+            preparedStatement.setString(4, tahun);
+            preparedStatement.setString(5, rak);
+            preparedStatement.setString(6, target);
+            int execute = preparedStatement.executeUpdate();
+            if (execute == 1) {
+                System.out.println("Update Berhasil");
+            }
+            else {
+                System.out.println("Update Gagal");
+            }
+        } catch (SQLException e) {
+            System.out.println("Update Gagal");
+            e.printStackTrace();
+        }
+    }
+
+    public void hapusData (String target) {
+        try {
+            preparedStatement = connection.prepareStatement("DELETE FROM databuku where `Judul Buku` = ?");
+            preparedStatement.setString(1, target);
+            int execute = preparedStatement.executeUpdate();
+            if (execute == 1) {
+                System.out.println("Data berhasil dihapus");
+            }
+            else {
+                System.out.println("Data tidak terhapus");
+            }
+        } catch (SQLException e) {
+            System.out.println("Data tidak terhapus");
+            e.printStackTrace();
+        }
+    }
+
+    public void selectData(String judulBuku) {
+        try {
+            preparedStatement = connection.prepareStatement("SELECT * FROM databuku WHERE `Judul Buku` = ?");
+            preparedStatement.setString(1, judulBuku);
+            int execute = preparedStatement.executeUpdate();
+            if (execute == 1) {
+                System.out.println("Data Ditemukan");
+            }
+            else {
+                System.out.println("Buku Yang Anda Cari Tidak Ditemukan");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
