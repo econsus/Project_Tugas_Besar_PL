e35427f90c536054de15b42b213db54c7a9adef0
KrakenAura <akarim18641@gmail.com>
2023-01-02 15:43:03 +0700

    Main_Menu Input Button Functional

--- src/Driver.java
+++ src/Driver.java
@@ -1,10 +1,12 @@
 public class Driver {
     public static void main(String[] args) {
+        Main_Screen mainScreen = new Main_Screen();
+        mainScreen.displayMainScreen(mainScreen);
 //    delete_buku deleteBuku = new delete_buku();
 //    deleteBuku.display(deleteBuku);
 //    deleteBuku.deleteButton();
-        Input_Data input_data = new Input_Data();
-        input_data.display(input_data);
-        input_data.input_Button();
+//        Input_Data input_data = new Input_Data();
+//        input_data.display(input_data);
+//        input_data.input_Button();
     }
 }
