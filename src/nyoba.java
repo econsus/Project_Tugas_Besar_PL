3d9f7c65abe3a82970f3838f485c73ada1275db2
Tazkia Damayanti <tazkiadamayanti@gmail.com>
2023-01-02 14:18:08 +0700

    nyoba aja 2

--- src/nyoba.java
+++ src/nyoba.java
@@ -1,6 +1,14 @@
 import javax.swing.*;
 
-public class nyoba {
+public class nyoba extends JFrame{
     private JButton button1;
     private JTable table1;
+    private JPanel panelA;
+
+    public void display(nyoba screen) {
+        screen.setContentPane(screen.panelA);
+        screen.setSize(400, 400);
+        screen.setVisible(true);
+        screen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 }
