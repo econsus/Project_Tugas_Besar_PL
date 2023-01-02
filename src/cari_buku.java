ce9eb6690e0df96e0592c3b6a745b4b100c81831
Hammam Abid <mha150720@gmail.com>
2023-01-02 15:42:03 +0700

    add display di cari_buku.java

--- src/cari_buku.java
+++ src/cari_buku.java
@@ -1,10 +1,12 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class cari_buku extends JFrame{
     private JTextField textField1;
     private JPanel cari_Buku;
     private JButton button;
     private JButton kembaliButton;
+    private DatabaseManager databaseManager = new DatabaseManager();
 
     public JPanel getCari_Buku() {
         return cari_Buku;
@@ -12,5 +14,13 @@ public class cari_buku extends JFrame{
 
     private void createUIComponents() {
         // TODO: place custom component creation code here
+    }
+
+    public void display(cari_buku screen) {
+        screen.setContentPane(cari_Buku);
+        screen.setSize(400, 400);
+        screen.setVisible(true);
+        screen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        databaseManager.connect();
     }
 }
