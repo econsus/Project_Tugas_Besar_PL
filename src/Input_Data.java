b79d67d0117a32493d4787d5efa149104bca9803
Tazkia Damayanti <tazkiadamayanti@gmail.com>
2023-01-02 14:41:01 +0700

    Input Data form

---
+++ src/Input_Data.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Input_Data {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JButton inputButton;
+    private JPanel Input_Data;
+}
